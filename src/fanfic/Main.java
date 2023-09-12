09834cfbb0073496c575004a3f4c0e6198344e6f
Matheus A <maf_dev@hotmail.com>
2023-09-12 20:33:27 -0300

    Initial commit

---
+++ src/fanfic/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fanfic;
+
+import fanfic.frontEnd.MainView;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MainView().setVisible(true);
+    }    
+}
---
+++ src/fanfic/backEnd/db/Conection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fanfic.backEnd.db;
+
+/**
+ *
+ * @author ma_fe
+ */
+public class Conection {
+    
+}
