80e804257214fb14442308a2c42c8aca47da398f
Matheus A <maf_dev@hotmail.com>
2023-09-14 17:35:55 -0300

    Add logic to show fic detail if lst is dbl clicked

---
+++ src/fanfic/backEnd/logic/Fanfic.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fanfic.backEnd.logic;
+
+/**
+ *
+ * @author ma_fe
+ */
+public class Fanfic {
+    private String name;
+    
+    public Fanfic (String n){
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+}
